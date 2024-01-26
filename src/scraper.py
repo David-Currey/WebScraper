d82748842dbf61de1148fb84590ed355b9323cf0
David-Currey <davecurrey76@gmail.com>
2024-01-26 17:45:29 -0500

    started implementing with selenium

--- src/scraper.py
+++ src/scraper.py
@@ -1,15 +1,34 @@
 import csv
+import getpass
+import time
 
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# import requests
+# from bs4 import BeautifulSoup
 
 # resource
-web_page = requests.get("https://quotes.toscrape.com")
-soup = BeautifulSoup(web_page.text, "html.parser")
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://quotes.toscrape.com")
+
+web_page.find_element(By.LINK_TEXT, "login").click()
+
+"""
+time.sleep(3)
+username = web_page.find_element(By.ID, "username")
+password = web_page.find_element(By.ID, "username")
+username.send_keys("admin")
+my_pass = getpass.getpass()
+password.send_keys(my_pass)
+web_page.find_element(By.CSS_SELECTOR, "input.btn-primary").click()
+"""
 
 # data
-quotes = soup.findAll("span", attrs={"class": "text"})
-authors = soup.findAll("small", attrs={"class": "author"})
+quotes = web_page.find_elements(By.CLASS_NAME, "text")
+authors = web_page.find_elements(By.CLASS_NAME, "author")
 
 # create csv
 file = open("scraped_data.csv", "w")
