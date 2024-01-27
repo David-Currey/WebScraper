3eb228d2717d2375fe49a03155bd39a3c8fa3e14
David-Currey <davecurrey76@gmail.com>
2024-01-27 00:21:44 -0500

    selenium version now running!

--- src/scraper.py
+++ src/scraper.py
@@ -7,28 +7,25 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-# import requests
-# from bs4 import BeautifulSoup
-
 # resource
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://quotes.toscrape.com")
 
-web_page.find_element(By.LINK_TEXT, "login").click()
+# driver.find_element(By.LINK_TEXT, "login").click()
 
 """
 time.sleep(3)
-username = web_page.find_element(By.ID, "username")
-password = web_page.find_element(By.ID, "username")
+username = driver.find_element(By.ID, "username")
+password = driver.find_element(By.ID, "password")
 username.send_keys("admin")
 my_pass = getpass.getpass()
 password.send_keys(my_pass)
-web_page.find_element(By.CSS_SELECTOR, "input.btn-primary").click()
+driver.find_element(By.CSS_SELECTOR, "input.btn-primary").click()
 """
 
 # data
-quotes = web_page.find_elements(By.CLASS_NAME, "text")
-authors = web_page.find_elements(By.CLASS_NAME, "author")
+quotes = driver.find_elements(By.CLASS_NAME, "text")
+authors = driver.find_elements(By.CLASS_NAME, "author")
 
 # create csv
 file = open("scraped_data.csv", "w")
@@ -40,3 +37,4 @@ for quote, author in zip(quotes, authors):
     print(quote.text + " - " + author.text)
     writer.writerow([quote.text, author.text])
 file.close()  # close csv
+driver.quit()  # close driver
